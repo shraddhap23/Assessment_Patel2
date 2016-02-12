09bdbef0bb520f655f20ea1b854c41155379d145
Shraddha Patel <spatelrb@gmail.com>
2016-02-12 16:04:54 -0500

    submission

---
+++ Assessment.js
@@ -0,0 +1,27 @@
+function countDown (time){
+	var timer = seconds;
+	if (timer<60) {
+		a
+
+
+	})
+
+}
+	
+
+// for (var i = 0; i < 60 ; i++){
+// 	console.log(i)
+// }
+
+
+
+
+// Create a function that initiates a countdown like you would use 
+// when baking cookies or telling someone they have 1 minute to speak 
+// on stage.  It takes in the number in seconds as its only argument 
+// and counts down to zero! Start with displaying the seconds in the 
+// console then try to get it to count down on your screen within an 
+// html element. 
+// If you have time to push this challenge further, style the 
+// background to change based on how many seconds are left! 
+// Note: Do not use the javascript setInterval() function.
